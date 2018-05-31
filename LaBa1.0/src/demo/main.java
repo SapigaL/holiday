037c5b756d6166470ee8fedf57b6e1e402b37f12
SapigaL <32940871+SapigaL@users.noreply.github.com>
2018-05-31 22:35:31 +0300

    Reformat code

---
+++ LaBa1.0/src/demo/main.java
@@ -0,0 +1,33 @@
+package demo;
+
+public class main {
+
+    public static void main(String[] args) {
+        Gym boxing = new Gym();
+        boxing.setName("Kick Him");
+        boxing.setArea(300);
+        boxing.setNabmerOfSimulators(32);
+        boxing.setType("Boxing");
+        boxing.setArea(642);
+
+        System.out.println(boxing.toString());
+        Gym.printStaticSum();
+
+        Gym weightlifting = new Gym("Fixsiki","Kachalka", 87,5 );
+        weightlifting.setArea(843);
+        System.out.println(weightlifting.toString());
+        Gym.printStaticSum();
+
+
+        Gym gym = new Gym ("Dynamo","pool", 1000, 120,6 );
+        System.out.println(gym.toString());
+        Gym.printStaticSum();
+
+
+        boxing.resetValues("Kick Him", "Boxing", 20,500 ,4);
+        System.out.println(boxing.toString());
+        Gym.printStaticSum();
+
+
+    }
+}
